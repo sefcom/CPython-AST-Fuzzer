8a8a2c5d463be5c75828c2eebeadd8b91358b1e4
eritque0arcus <eritque-arcus@ikuyo.dev>
2024-07-05 16:14:37 -0700

    chore: update script to consist w/ nix-pyenv

--- scripts/hints.nix
+++ scripts/hints.nix
@@ -13,8 +13,14 @@ pkgs.mkShell {
   packages = llvm_pkgs ++ [
     pyenv
   ];
-  shellHook = ''
+shellHook = ''
+    cd ${builtins.toString ./.}
+
+    # ensure the nix-pyenv directory exists
     if [[ ! -d ${nix_pyenv_directory} ]]; then mkdir ${nix_pyenv_directory}; fi
+    if [[ ! -d ${nix_pyenv_directory}/lib ]]; then mkdir ${nix_pyenv_directory}/lib; fi
+    if [[ ! -d ${nix_pyenv_directory}/bin ]]; then mkdir ${nix_pyenv_directory}/bin; fi
+
     ensure_symlink() {
         local link_path="$1"
         local target_path="$2"
@@ -25,16 +31,33 @@ pkgs.mkShell {
         ln -s "$target_path" "$link_path"
     }
 
+    # creating python library symlinks
     for file in ${pyenv}/${python_custom_plain.sitePackages}/*; do
-        ensure_symlink ${nix_pyenv_directory}/$(basename $file) $file
+        basefile=$(basename $file)
+        if [ -d "$file" ]; then
+            if [[ "$basefile" != *dist-info && "$basefile" != __pycache__ ]]; then
+                ensure_symlink ${nix_pyenv_directory}/lib/$basefile $file
+            fi
+        else
+            # the typing_extensions.py will make the vscode type checker not working!
+            if [[ $basefile == *.so ]] || ([[ $basefile == *.py ]] && [[ $basefile != typing_extensions.py ]]); then
+                ensure_symlink ${nix_pyenv_directory}/lib/$basefile $file
+            fi
+        fi
     done
-    for file in ${nix_pyenv_directory}/*; do
+    for file in ${nix_pyenv_directory}/lib/*; do
         if [[ -L "$file" ]] && [[ "$(dirname $(readlink "$file"))" != "${pyenv}/${python_custom_plain.sitePackages}" ]]; then
             rm -f "$file"
         fi
     done
-    ensure_symlink ${nix_pyenv_directory}/python ${pyenv}/bin/python
+
+    # ensure the typing_extensions.py is not in the lib directory
+    rm ${nix_pyenv_directory}/lib/typing_extensions.py > /dev/null 2>&1
+
+    # add python executable to the bin directory
+    ensure_symlink ${nix_pyenv_directory}/bin/python ${pyenv}/bin/python
+    export PATH=${nix_pyenv_directory}/bin:$PATH
     export CPYTHON_INCLUDE=${python_custom_plain}/include/python3.11;
-    export CLANG_BIN=${clang}/bin/clang;
+    export CLANG_BIN=${clang}/bin/clang
   '';
 }
