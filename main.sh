9656c3e4cfd0a981b634eaea466f00efd215331c
eritque0arcus <eritque-arcus@ikuyo.dev>
2024-07-09 20:19:07 -0700

    chore: add script support

--- main.sh
+++ main.sh
@@ -13,11 +13,11 @@ popd() {
     command popd "$@" >/dev/null
 }
 
-SCRIPT_DIR=$(readlink -f ./scripts)
 BUILD_PATH=$(readlink -f ./build)
-SRC_PATH=$(readlink -f ./src)
-CPYTHON_VERSION=3.11.9
+LOG_PATH=$(readlink -f .)/log$(date +"%H%M%S")
 
-pushd $BUILD_PATH
-nix-shell --pure --command "LD_PRELOAD="\$PYTHON_PKGS_PATH/asan_with_fuzzer.so" \$PYTHON_PATH/bin/python $SRC_PATH/main.py -atheris_runs=5" $SCRIPT_DIR/cpython.nix --argstr py_ver_str $CPYTHON_VERSION
+mkdir -p $LOG_PATH
+
+pushd $LOG_PATH
+$BUILD_PATH/pyFuzzerHelper &> $LOG_PATH/log.txt
 popd
